54f83c3eb25345c2c1b290b548b491757bb2b6c0
=Gaspare <=gbonventre@gmail.com>
2018-12-21 15:54:57 -0500

    Added initial install file

---
+++ InitialInstallation.ps1
@@ -0,0 +1,10 @@
+﻿
+#Get-ExecutionPolicy
+# Enable scripts to run on this system
+Set-ExecutionPolicy unrestricted
+
+# Install Powershell Get to facilitate installing packages from PowerShellGallery.com
+Install-Module -Name PowerShellGet -Force
+
+#Installation of Azure Active Directory Module
+Install-Module -Name AzureAD -Force
